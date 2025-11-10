b9af23acec1a4f604f5b7776ab96ce0c84b7b97a
sudhesh.holla <sudheshholla15@gmail.com>
2025-11-10 22:26:41 +0530

    embedding.js

---
+++ embedding.js
@@ -0,0 +1,11 @@
+import 'dotenv/config';
+import OpenAI from "openai";
+const openai = new OpenAI();
+
+const embedding = await openai.embeddings.create({
+  model: "text-embedding-3-small",
+  input: "My name is Sudhesh",
+  encoding_format: "float",
+});
+
+console.log(embedding.data[0].embedding.length);
